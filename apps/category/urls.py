fce7a8effa9b2ac5f87baf002eef43b0ec708f67
Gerdava2602 <germandavidvargas@hotmail.com>
2023-01-28 13:10:56 -0500

    react initializeed, redux applied and backend finished

--- apps/category/urls.py
+++ apps/category/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-
 from .views import *
 
 urlpatterns = [
+    path('categories', ListCategoriesview.as_view(), name='category_list'),
 ]
---
+++ src/containers/errors/Error404.jsx
@@ -0,0 +1,13 @@
+import FullWidthLayout from "hocs/layouts/FullWidthLayout"
+
+function Error404(){
+    return(
+        <div>
+            <FullWidthLayout>
+                Error
+            </FullWidthLayout>
+        </div>
+    )
+}
+
+export default Error404
